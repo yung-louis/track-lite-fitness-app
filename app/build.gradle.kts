06506697f35ef3eabf14a1f636c47d5300255bef
yung-louis <louismyung@gmail.com>
2024-05-14 16:52:41 +0100

    updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,7 +31,7 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
     kotlinOptions {
